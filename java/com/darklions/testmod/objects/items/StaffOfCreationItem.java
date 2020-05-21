8cf1a3f68c7bdb761f39d73b3edd4923280a7384
DarkLions <57546807+DarkLions-YT@users.noreply.github.com>
2020-05-21 13:58:00 +0200

    Update StaffOfCreationItem.java

--- java/com/darklions/testmod/objects/items/StaffOfCreationItem.java
+++ java/com/darklions/testmod/objects/items/StaffOfCreationItem.java
@@ -16,21 +16,15 @@ import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceContext.FluidMode;
 import net.minecraft.util.math.RayTraceResult;
-import net.minecraft.util.text.ITextComponent;
-import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.world.World;
 
 public class StaffOfCreationItem extends Item
 {
-	private static int cubeSize = 3;
-	private static BlockState Block = Blocks.STONE.getDefaultState();
-	
 	public StaffOfCreationItem(Item.Properties properties)
 	{
 		super(properties);
 	}
 	
-	@SuppressWarnings("static-access")
 	@Override
 	public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) 
 	{
@@ -42,7 +36,7 @@ public class StaffOfCreationItem extends Item
 		}
 		else
 		{
-			RayTraceResult result = CustomRayTraceResult.CustomrayTrace(worldIn, playerIn, FluidMode.SOURCE_ONLY, 15D);
+			RayTraceResult result = CustomRayTraceResult.CustomrayTrace(worldIn, playerIn, FluidMode.SOURCE_ONLY, 40D);
 			
 			BlockPos TargetPos = new BlockPos(result.getHitVec().x, result.getHitVec().y, result.getHitVec().z);
 			BlockPos playerPos = playerIn.getPosition().down();
@@ -54,16 +48,11 @@ public class StaffOfCreationItem extends Item
 			
 			List<Vec3> vecs = calc.CoordListVec3();
 			
-			playerIn.sendMessage((new StringTextComponent(playerPos.toString())));
-			playerIn.sendMessage((new StringTextComponent(TargetPos.toString())));
-			
-			//worldIn.setBlockState(TargetPos, Blocks.STONE.getDefaultState());
-			
 			for(Vec3 vec : vecs)
 			{
-				worldIn.setBlockState(new BlockPos(vec.X(), vec.Y(), vec.Z()), Blocks.STONE.getDefaultState());
-				System.out.println(vec.X());
-				System.out.println(vec.Z());
+				BlockPos pos = new BlockPos(vec.X(), vec.Y(), vec.Z());
+
+				worldIn.setBlockState(pos, Blocks.COBBLESTONE.getDefaultState());	
 			}
 			
 			return ActionResult.resultSuccess(itemStack);
